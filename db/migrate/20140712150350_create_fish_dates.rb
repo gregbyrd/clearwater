eca0a9d07fce0affbd86c598c7c709822771c40d
Greg Byrd <gbyrd.ncsu@gmail.com>
2014-07-12 18:18:05 +0200

    fixed foreign key season_id in FishDate

--- db/migrate/20140712150350_create_fish_dates.rb
+++ db/migrate/20140712150350_create_fish_dates.rb
@@ -2,7 +2,7 @@ class CreateFishDates < ActiveRecord::Migration
   def change
     create_table :fish_dates do |t|
       t.date :day
-      t.references :season_id, index: true
+      t.references :season, index: true
 
       t.timestamps
     end
