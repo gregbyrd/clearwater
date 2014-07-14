f1214f20978a58af858fc69c249a3b1d610d687b
Greg Byrd <gbyrd.ncsu@gmail.com>
2014-07-14 21:45:56 +0200

    added sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ActiveRecord::Base
 
   has_many :slots
   has_many :fish_dates, through: :slots
+  validates_presence_of :password_digest
   validates_presence_of :admin
-  validates :email, presence: true, :uniqueness: {case_sensitive: false} 
+  validates :email, presence: true, uniqueness: {case_sensitive: false} 
   validates_format_of :email,
     with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Clearwater::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resource :session, only: [:new, :create, :destroy]
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
