7eec73a9936b8dae3ccafd4ad1f34274798b4b38
Greg Byrd <gbyrd.ncsu@gmail.com>
2014-07-14 20:55:01 +0200

    serialize guests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :password_digest, :admin
   validates_format_of :email,
     with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/
+
+  serialize :guests, Array
+
 end
