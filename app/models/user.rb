23ac5861c966ab39ad72745cfbee7dcaa80c18d5
Greg Byrd <gbyrd.ncsu@gmail.com>
2014-07-14 21:03:46 +0200

    add has_secure_password to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   has_many :slots
   has_many :fish_dates, through: :slots
-  validates_presence_of :email, :password_digest, :admin
+  validates_presence_of :admin
+  validates :email, presence: true, :uniqueness: {case_sensitive: false} 
   validates_format_of :email,
     with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/
 
