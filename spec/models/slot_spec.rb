e5b39bcf1d81943bd090f84e97516891e3f4d718
Greg Byrd <gbyrd.ncsu@gmail.com>
2014-07-14 16:56:23 +0200

    test for slot

--- spec/models/slot_spec.rb
+++ spec/models/slot_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Slot, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Slot, :type => :model do
+  describe 'validations' do
+    it { should validate_presence_of :label }
+  end
 end
