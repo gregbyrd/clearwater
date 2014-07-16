c5f0e7a5c8f163ca72f28757220624e893ddc1bc
Greg Byrd <gbyrd.ncsu@gmail.com>
2014-07-16 04:27:37 +0200

    season views and actions

--- app/controllers/admin/seasons_controller.rb
+++ app/controllers/admin/seasons_controller.rb
@@ -1,2 +1,21 @@
 class Admin::SeasonsController < ApplicationController
+  def index
+    @seasons = Season.all
+  end
+
+  def show
+    @season = Season.find(params[:id])
+    @dates = @season.fish_dates
+  end
+
+  def new
+  end
+
+  def create
+    season = Season.create!(year: params[:date][:year].to_i,
+                            start_month: params[:date][:start_month].to_i,
+                            end_month: params[:date][:end_month].to_i,
+                            slot_limit: params[:slot_limit].to_i)
+    redirect_to admin_seasons_path
+  end
 end
